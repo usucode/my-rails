ec677f6f30db4765bc88c793f3844c0c6193732b
Yusuke Akiyama <akiyamaysk0321@gmail.com>
2023-01-31 00:18:24 +0900

    init

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'top#index'
+end
